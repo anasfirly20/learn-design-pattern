a222ed333c117b2525114248a539ce3e0e0e5db4
Anas Firly <anasfirly20@gmail.com>
2023-12-03 16:00:40 +0700

    feat: adding crud logic todo

--- src/app/todo/components/AddItem.tsx
+++ src/app/todo/components/AddItem.tsx
@@ -4,14 +4,18 @@ import { Input } from "@nextui-org/input";
 import { useTodo } from "../functions";
 
 export default function AddItem() {
-  const { handleAdd, handleChange, newTodo, handleKeyDown } = useTodo();
+  const { handleAdd, handleChange, newTodo } = useTodo();
   return (
     <>
       <Input
         isClearable
         value={newTodo}
         onChange={(e) => handleChange(e)}
-        onKeyDown={(e) => handleKeyDown(e)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleAdd(newTodo);
+          }
+        }}
       />
       <Button color="primary" onClick={() => handleAdd(newTodo)}>
         Submit
